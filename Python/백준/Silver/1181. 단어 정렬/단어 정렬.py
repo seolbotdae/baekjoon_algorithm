e2e2410ee65b6cbd8135aa3714c2bf33a21bde31
Hututi <seolbotdae@gmail.com>
2024-04-26 21:21:24 +0900

    [Silver V] Title: 단어 정렬, Time: 860 ms, Memory: 36756 KB -BaekjoonHub

--- Python/백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ Python/백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -6,7 +6,7 @@ for _ in range(n):
     word = input()
     wordsSet.add((len(word), word))
     
-
+# 튜플을 정렬할 경우, 앞의 원소를 기준으로 정렬하고, 앞의 원소끼리 같을 경우 뒤의 원소를 기준으로 정렬
 result = sorted(wordsSet)
 
 for i, word in result:
